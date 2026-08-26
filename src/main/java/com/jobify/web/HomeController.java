b9614d311794b6607de7035697e2d24b01602aef
sam <sam837847@gmail.com>
2026-08-26 20:23:38 +0530

    Initial commit

---
+++ src/main/java/com/jobify/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.jobify.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Jobify is running";
+    }
+}
